bb4cdffc33e107353282075a87a33ac8d14fed10
Armaan Aggarwal <49625265+armaan115@users.noreply.github.com>
2019-05-10 18:22:23 -0700

    cxgen 1.0 uploaded!

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # cxgen
-A package to generate cx_Freeze setup.py file!
+v1.0 Created by Armaan Aggarwal on April 7, 2019
+
+This is a little package that can generate a cx_Freeze setup.py script 
+to 
+turn your program into an .exe or .app file!
+
+
+
+To know how to use this module, import cxgen and then call: 'cxgen.manual()'
+
+PyPI page: https://pypi.org/project/cxgen/
+GitHub repository: https://github.com/armaan115/cxgen
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cxgen",
+    version="1.0", 
+    author="Armaan Aggarwal",
+    description="A package to generate cx_Freeze setup.py file!",
+    long_description=long_description,
+    packages=setuptools.find_packages(),
+
+    data_files=[('', ['cxgen/files/manual.txt'])],
+##    include_package_data=True,
+    
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
