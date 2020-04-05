e7da3470d0a0c78f18ffde4046871edfe539448a
Armaan <armaanaggarwal@gmail.com>
2020-04-05 13:22:59 -0700

    allow repo to be cloned from PIP

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cxgen",
+    version="3.0.0", 
+    author="Armaan Aggarwal",
+    description="A package to generate cx_Freeze setup.py file!",
+    python_requires='>=3, <4',
+    long_description=long_description,
+    packages=setuptools.find_packages(),
+    license='MIT',
+    url = "https://github.com/armaan115/cxgen",
+
+    include_package_data=True,
+    
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
